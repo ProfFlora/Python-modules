e19665cacc14f064f669d94f4bf58f6cee8eafbf
Flora Cauli <cauli_2004@usp.br>
2026-08-14 01:27:33 -0300

    miau

--- module2/ex0/ft_first_exception.py
+++ module2/ex0/ft_first_exception.py
@@ -2,7 +2,7 @@ def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
 
-def test_temperature():
+def test_temperature() -> None:
     print("=== Garden Temperature ===\n")
     for string in ("25", "abc"):
         print(f"Input data is '{string}'")
