3e2300d9f3c367cc4e507feb8adb4d8e91d8e4b5
Flora Cauli <cauli_2004@usp.br>
2026-08-14 01:26:34 -0300

    miau

---
+++ module2/ex0/ft_first_exception.py
@@ -0,0 +1,20 @@
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature():
+    print("=== Garden Temperature ===\n")
+    for string in ("25", "abc"):
+        print(f"Input data is '{string}'")
+        try:
+            temp = input_temperature(string)
+        except ValueError:
+            print("Caught input_temperature error: ", end="")
+            print("invalid literal for int() with base 10: '{string}'\n")
+        else:
+            print(f"Temperature is now {temp}ºC\n")
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    test_temperature()
